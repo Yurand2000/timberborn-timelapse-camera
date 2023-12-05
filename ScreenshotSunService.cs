78cb42b3322035fe29be7f33bf6147c86fda01f1
Yuri Andriaccio <yurand2000@gmail.com>
2023-12-05 21:59:56 +0100

    Updates for Timberborn 0.5.4.0

--- ScreenshotSunService.cs
+++ ScreenshotSunService.cs
@@ -3,59 +3,35 @@ using System.Reflection;
 using Timberborn.SingletonSystem;
 using TimberApi.DependencyContainerSystem;
 using TimberApi.ConsoleSystem;
+using Timberborn.SkySystem;
 using UnityEngine;
 
 namespace Yurand.Timberborn.TimelapseCamera
 {
     public class ScreenshotSunService : ILoadableSingleton
     {
-        private MethodInfo sunUpdateMethod;
         private MethodInfo sunUpdateColorsMethod;
         private FieldInfo sunLightField;
 
-
-        private object sunSingleton;
-        private object dayStageTransition;
+        private DayStageTransition dayStageTransition;
         private IConsoleWriter console;
+        private Sun sun;
 
-        public ScreenshotSunService(IConsoleWriter console) {
+        public ScreenshotSunService(IConsoleWriter console, Sun sun) {
             this.console = console;
+            this.sun = sun;
         }
 
         public void Load()
         {
-            var sunClassType = AccessTools.TypeByName("Timberborn.SkySystem.Sun")
-                ?? throw new System.Exception("ScreenshotSunService: type Timberborn.SkySystem.Sun not found");
-            var dayStageType = AccessTools.TypeByName("Timberborn.SkySystem.DayStage")
-                ?? throw new System.Exception("ScreenshotSunService: type Timberborn.SkySystem.DayStage not found");
-            var dayStageTransitionType = AccessTools.TypeByName("Timberborn.SkySystem.DayStageTransition")
-                ?? throw new System.Exception("ScreenshotSunService: type Timberborn.SkySystem.DayStageTransition not found");
-
-            sunUpdateMethod = AccessTools.Method(sunClassType, "Update")
-                ?? throw new System.Exception("ScreenshotSunService: method Update not found");
+            var sunClassType = typeof(Sun);
 
             sunUpdateColorsMethod = AccessTools.Method(sunClassType, "UpdateColors", new System.Type[]{
-                dayStageTransitionType
+                typeof(DayStageTransition)
             }) ?? throw new System.Exception("ScreenshotSunService: method UpdateColors not found");
 
-            var dayStageTransitionConstructor = AccessTools.Constructor(
-                dayStageTransitionType,
-                new System.Type[]{
-                    dayStageType,
-                    typeof(bool),
-                    dayStageType,
-                    typeof(bool),
-                    typeof(float)
-                }
-            ) ?? throw new System.Exception("ScreenshotSunService: dayStageTransition constructor not found");
+            dayStageTransition = new DayStageTransition(DayStage.Day, "", DayStage.Day, "", 0.0f);
 
-            var dayStageDay = System.Enum.GetValues(dayStageType).GetValue(1)
-                ?? throw new System.Exception("ScreenshotSunService: dayStage enum as day not found");
-            dayStageTransition = dayStageTransitionConstructor.Invoke(new object[] {dayStageDay, false, dayStageDay, false, 0.0f})
-                ?? throw new System.Exception("ScreenshotSunService: dayStageTransition not constructed correctly");
-
-            sunSingleton = DependencyContainer.GetInstance(sunClassType)
-                ?? throw new System.Exception("ScreenshotSunService: Sun singleton not found");
             sunLightField = AccessTools.Field(sunClassType, "_sun")
                 ?? throw new System.Exception("ScreenshotSunService: Sun light field not found");
 
@@ -64,12 +40,12 @@ namespace Yurand.Timberborn.TimelapseCamera
         }
 
         public void setSunForScreenshotRendering(Camera camera) {
-            sunUpdateColorsMethod.Invoke(sunSingleton, new object[]{ dayStageTransition });
-            ((Light) sunLightField.GetValue(sunSingleton)).transform.rotation = camera.transform.rotation;
+            sunUpdateColorsMethod.Invoke(sun, new object[]{ dayStageTransition });
+            ((Light) sunLightField.GetValue(sun)).transform.rotation = camera.transform.rotation;
         }
 
         public void resetSunForScreenshotRendering() {
-            sunUpdateMethod.Invoke(sunSingleton, null);
+            sun.Update();
         }
     }
 }
